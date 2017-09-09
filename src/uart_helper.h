5d5ae409a149b3b0feaa0236a9de0328d06ef6d6
sirhutson <31014881+sirhutson@users.noreply.github.com>
2017-09-08 21:06:41 -0400

    work on Uart
    added switch case to be used by the main for so that the menu of uart can return. need to ask questions regarding uart.

--- src/uart_helper.h
+++ src/uart_helper.h
@@ -1,7 +1,14 @@
-#include "../driverlib/sysctl.h"
-#include "../driverlib/gpio.h"
-#include "../inc/hw_memmap.h"
-#include "../inc/hw_types.h"
-
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include "sysctl.h"
+#include "gpio.h"
+#include "hw_memmap.h"
+#include "hw_types.h"
+#include "hw_gpio.h"
+#include "uart.h"
+#include "pin_map.h"
 
 void Uart_setup(void);
+void process_Uart(void);
+void print_start_msg(void);
