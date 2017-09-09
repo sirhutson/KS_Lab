f73c37670e399635bb626fbd6ab5bca94116df3f
sirhutson <31014881+sirhutson@users.noreply.github.com>
2017-09-08 21:39:17 -0400

    project.c changes
    project now interacts with user to a degree

--- project.c
+++ project.c
@@ -4,31 +4,36 @@
 
 int main()
 {
-			Gpio_setup() ; 
+		Gpio_setup();
+	
+		//insert function that blinks red light specified number of times as if it were booting the system up
 
-			while(1){
-				if (GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_0)==0x00){
-					
-			GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3,GPIO_PIN_3);
+			while(GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_0)!=0x00)  // waits for the user to press start
+			{
+				//if (GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_0)!=0x00)
+					//{
+						GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3,GPIO_PIN_3); // gives user the green light start touching stuff
+					//}
 			}
-		}
+		GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3,0); // cuts off green light
+		GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2,GPIO_PIN_2);  // blue light indicates user has begun touching stuff and process is under way
 		
 }
 
 
-void read_from_terminal()
-{
-		switch()
-		{
-			case a:
-			//input whatever the case and then end line
-			break;
-			case b:
-			//input whatever the case and then end line
-			break;
-		
-		}
+//void read_from_terminal()
+//{
+//		switch()
+//		{
+//			case a:
+//			//input whatever the case and then end line
+//			break;
+//			case b:
+//			//input whatever the case and then end line
+//			break;
+//		
+//		}
 
-	
-	
-}
+//	
+//}	
+
