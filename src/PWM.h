69dfdb7d43c77c8e211e1ffad2f76b2cc3189eea
ktorres24 <31192254+ktorres24@users.noreply.github.com>
2017-09-27 11:48:25 -0400

    PWM
    Added PWM that should fade from
    red-> purple -> yellow 10 times before returning to main.
    Been having issues with main actually calling the function instead of skipping over it.

---
+++ src/PWM.h
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include "hw_ints.h"
+#include "hw_timer.h"
+#include "hw_gpio.h"
+#include "hw_memmap.h"
+#include "pin_map.h"
+#include "sysctl.h"
+#include "timer.h"
+#include "gpio.h"
+
+#define time 56666
+
+
+void PWM_setup(void);
+void PWM_example(void);
